937709709459d2f3336d1f8f0b6ee1d96bc5136b
uoay <67417191+uoay@users.noreply.github.com>
2025-03-04 17:28:36 +0800

    feat: websocket echo service

---
+++ src/main/java/net/uoay/chat/config/package-info.java
@@ -0,0 +1,3 @@
+@org.springframework.lang.NonNullApi
+@org.springframework.lang.NonNullFields
+package net.uoay.chat.config;
---
+++ src/main/java/net/uoay/chat/websocket/EchoHandler.java
@@ -0,0 +1,16 @@
+package net.uoay.chat.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class EchoHandler extends TextWebSocketHandler {
+
+    @Override
+   	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        session.sendMessage(message);
+	}
+
+}
